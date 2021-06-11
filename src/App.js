b2c185c067d523fc687ce11e32cad7c14a73bf6b
Oleksii Bohatyrenko <steenbras31@gmail.com>
2021-06-11 14:43:16 +0300

    Попытка закоммитить номер 2 с ошибками

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Profile from './components/Profile';
+// import Profile from './components/Profile';
 import Statistics from './components/Statistics';
 import Friends from './components/FriendList';
 import TransactionHistory from './components/TransactionHistory';
@@ -8,18 +8,18 @@ import statisticalData from './statistical-data.json';
 import friendsData from './friends.json';
 import transactionData from './transaction.json';
 
-const colorPickerOptions = [
-  { label: 'red', color: '#F44336' },
-  { label: 'green', color: '#4CAF50' },
-  { label: 'blue', color: '#2196F3' },
-  { label: 'grey', color: '#607D8B' },
-  { label: 'pink', color: '#E91E63' },
-  { label: 'indigo', color: '#3F51B5' },
-];
+// const colorPickerOptions = [
+//   { label: 'red', color: '#F44336' },
+//   { label: 'green', color: '#4CAF50' },
+//   { label: 'blue', color: '#2196F3' },
+//   { label: 'grey', color: '#607D8B' },
+//   { label: 'pink', color: '#E91E63' },
+//   { label: 'indigo', color: '#3F51B5' },
+// ];
 
 const App = () => (
   <div>
-    <Profile data={userDate} />
+    <Profile data={userDate123} />
     <Statistics title="Upload stats" stats={statisticalData} />
     <Friends bestPeople={friendsData} />
     <TransactionHistory items={transactionData} />
